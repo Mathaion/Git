813e73e44810b6e1bf391d29552055131e7a0f53
Mathaion <72054014+Mathaion@users.noreply.github.com>
2022-10-30 09:34:18 -0300

    Update function.js
    slider

--- function.js
+++ function.js
@@ -1,9 +1,15 @@
+ function hola() {
+    var ob = document.getElementById("title")
+    if (ob.innerHTML == "Hola") {
+        ob.innerHTML = "Chau";}
+    else {ob.innerHTML = "Hola";}
+    }
 var num = 0
-var images = ["https://runda.si/i/1486182b-b00a-4f19-b799-eb3bbeedc9d2.jpg", 
-"https://www.stagioninelpiatto.com/wp-content/uploads/2019/11/pasta-ragu-di-lenticchie.jpg", 
-"https://bioradar.net/wp-content/uploads/2016/01/IMG_69221-1024x831.jpg", 
-"https://1.bp.blogspot.com/-YsNZdsZLm3k/Tlvk6VPfFmI/AAAAAAAAAVk/hV-Py5AnUIg/s1600/finepix+29+august+2011+099.JPG", 
-"https://blog.giallozafferano.it/pentolinesglutinate/wp-content/uploads/2021/03/20210305_133441-scaled.jpg"]
+var images = ["https://www.tasteofhome.com/wp-content/uploads/2018/01/Slow-Simmered-Meat-Ragu_exps59435_SD142780C08_16_3bC_RMS-1.jpg", 
+"https://skinnyandtasty.com/wp-content/uploads/2021/04/Italian-Beef-Ragu.jpg", 
+"https://i2.wp.com/foxeslovelemons.com/wp-content/uploads/2014/01/Lentil-Mushroom-Ragu1-1.jpg", 
+"https://thewoodenskillet.com/wp-content/uploads/2020/12/beef-ragu-recipe-5-2.jpg", 
+"https://sodelicious.recipes/wp-content/uploads/2017/10/c9762-25.05.2016-r1-ragu-sausage-pork-and-beef-ragu.jpg"]
 function next() {
     var slider = document.getElementById("change");
     num++
@@ -21,3 +27,33 @@ function prev() {
     }
     slider.src=images[num];
 }
+
+
+const button = document.getElementById('boton');
+const validacion = (e) => {
+  e.preventDefault();
+  const nombres = document.getElementById('name');
+  const correo = document.getElementById('email');
+  if (name.value === "") {
+    alert("Por favor, escribe tu nombre de usuario.");
+    nombres.focus();
+    return false;
+  }    
+  if (email.value === "") {
+    alert("Por favor, escribe tu correo electrónico");
+    email.focus();
+    return false;
+  }
+  if (!emailValido(email.value)) {
+    alert("Por favor, escribe un correo electrónico válido");
+    emailAddress.focus();
+    return false;
+  }  
+  return true;
+}
+const emailValido = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+submitBtn.addEventListener('click', validate);
+
+
